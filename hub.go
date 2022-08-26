d40a84ce49704b190c8024444f36b732a3840c07
nimoc <nimocc@126.com>
2022-08-26 13:49:31 +0800

    Update hub.go

--- hub.go
+++ hub.go
@@ -10,11 +10,19 @@ import (
 
 // Hub 利用 Store 生成 sessionID 和查找 Session
 type Hub struct {
-	store Store
+	store  Store
 	option HubOption
 }
+
 func NewHub(store Store, option HubOption) (hub *Hub, err error) {
+	// 默认 sesison ttl
+	if option.SessionTTL == 0 {
+		option.SessionTTL = time.Hour * 8
+	}
 	// http cookie name 默认值
+	if option.Cookie.MaxAge == 0 {
+		option.Cookie.MaxAge = int(option.SessionTTL.Seconds())
+	}
 	if option.Cookie.Name == "" {
 		option.Cookie.Name = "session_id"
 	}
@@ -29,22 +37,18 @@ func NewHub(store Store, option HubOption) (hub *Hub, err error) {
 	if option.Security == nil {
 		option.Security = DefaultSecurity{}
 	}
-	switch  option.Security.(type) {
+	switch option.Security.(type) {
 	case DefaultSecurity:
 		if len(option.SecureKey) != 32 {
 			return nil, xerr.New("goclub/sesison:  NewHub(store, option) option.SecureKey length must be 32")
 		}
-	}
-	// 默认 sesison ttl
-	if option.SessionTTL == 0 {
-		option.SessionTTL = time.Hour * 2
 	}
 	if store == nil {
 		return nil, xerr.New("goclub/sesison: NewHub(store, option) store can not be nil")
 	}
 
 	hub = &Hub{
-		store: store,
+		store:  store,
 		option: option,
 	}
 	return hub, nil
@@ -55,7 +59,7 @@ type HubOption struct {
 	SecureKey []byte
 	// cookie 相关设置
 	Cookie HubOptionCookie
-	// sesison 过期时间，默认2小时
+	// sesison 过期时间，默认8小时
 	SessionTTL time.Duration
 	// header 相关设置
 	Header HubOptionHeader
@@ -77,33 +81,36 @@ type HubOptionCookie struct {
 	// Path 默认为 "/"
 	Path   string
 	Domain string
-	MaxAge   int
-	Secure   bool
+	// Path 默认为 HubOption{}.SessionTTL
+	MaxAge int
+	Secure bool
 }
 type HubOptionHeader struct {
 	// Key 建议设置为 session  (若留空则为 session)
 	Key string
 }
 
-
 // 微信小程序和 app 场景下可能在登录成功时可能需要手动创建 SessionID
 // 所以提供 NewSessionID 发放
 func (hub Hub) NewSessionID(ctx context.Context) (sessionID string, err error) {
-	storeKey :=  uuid.New().String()
+	storeKey := uuid.New().String()
 	var sessionIDBytes []byte
-	sessionIDBytes, err = hub.option.Security.Encrypt([]byte(storeKey), hub.option.SecureKey) ; if err != nil {
+	sessionIDBytes, err = hub.option.Security.Encrypt([]byte(storeKey), hub.option.SecureKey)
+	if err != nil {
 		return
 	}
-	sessionID =  string(sessionIDBytes)
-	err = hub.store.InitSession(ctx, storeKey, hub.option.SessionTTL) ; if err != nil {
+	sessionID = string(sessionIDBytes)
+	err = hub.store.InitSession(ctx, storeKey, hub.option.SessionTTL)
+	if err != nil {
 		return
 	}
 	return sessionID, nil
 }
 
 func (hub Hub) GetSessionBySessionID(ctx context.Context, sessionID string) (session Session, sessionExpired bool, err error) {
-	session, has, err := hub.getSession(ctx, sessionID, nil) ; if err != nil {
-	    return
+	session, has, err := hub.getSession(ctx, sessionID, nil)
+	if err != nil {
+		return
 	}
 	// GetSessionBySessionID 的场景一般是微信小程序或 app
 	// 这种场景当 key 错误应当返回key已过期 ，便于调用方告知客户端需要重新登录
@@ -122,29 +129,33 @@ func (hub Hub) getSession(ctx context.Context, sessionID string, writer http.Res
 		return Session{}, false, nil
 	}
 	var storeKeyBytes []byte
-	storeKeyBytes, err = hub.option.Security.Decrypt([]byte(sessionID), hub.option.SecureKey) ; if err != nil {
+	storeKeyBytes, err = hub.option.Security.Decrypt([]byte(sessionID), hub.option.SecureKey)
+	if err != nil {
 		return Session{}, false, err
 	}
 	storeKey = string(storeKeyBytes)
 	session = Session{
 		sessionID: sessionID,
-		storeKey: storeKey,
-		hub: hub,
-		writer: writer,
+		storeKey:  storeKey,
+		hub:       hub,
+		writer:    writer,
 	}
 	// 此处的验证可避免 key 过期或恶意猜测key进行攻击
-	has, err = session.existed(ctx) ; if err != nil {
+	has, err = session.existed(ctx)
+	if err != nil {
 		return
 	}
 	if has == false && hub.option.OnStoreKeyDoesNotExist != nil {
 		hub.option.OnStoreKeyDoesNotExist(ctx, sessionID, storeKey)
 	}
 	// 实现自动续期
-	remainingTTL, err := session.hub.store.StoreKeyRemainingTTL(ctx, session.storeKey) ; if err != nil {
+	remainingTTL, err := session.hub.store.StoreKeyRemainingTTL(ctx, session.storeKey)
+	if err != nil {
 		return
 	}
-	if remainingTTL < session.hub.option.SessionTTL / 2 {
-		err = session.hub.store.RenewTTL(ctx, session.storeKey, session.hub.option.SessionTTL) ; if err != nil {
+	if remainingTTL < session.hub.option.SessionTTL/2 {
+		err = session.hub.store.RenewTTL(ctx, session.storeKey, session.hub.option.SessionTTL)
+		if err != nil {
 			return
 		}
 	}
